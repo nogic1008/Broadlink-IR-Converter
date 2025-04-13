83a1adbd64337fadaa960164b08123d117838c25
Nogic <24802730+nogic1008@users.noreply.github.com>
2025-04-13 09:44:28 +0900

    feat: init nuxt (#1)

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui-pro'],
+  css: ['~/assets/css/main.css'],
+})
